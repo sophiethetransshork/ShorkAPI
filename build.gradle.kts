7bfc8262486212861a852ac2885f23d4a3ea47ef
NotPiotrekDev <notpiotrekdev@gmail.com>
2024-12-17 20:03:55 +0100

    some changes for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "pl.sophietheshork.shorkapi"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -12,6 +13,37 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("ShorkAPI") {
+            from(components["java"])
+            pom {
+                name.set("ShorkAPI")
+                description.set("Library for my Minecraft plugins")
+                url.set("https://github.com/sophiethetransshork/ShorkAPI")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("sophietheshork")
+                        name.set("SophieTheShork")
+                        email.set("sophietheshork@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/sophiethetransshork/ShorkAPI.git")
+                    developerConnection.set("scm:git:ssh://github.com/sophiethetransshork/ShorkAPI.git")
+                    url.set("https://github.com/sophiethetransshork/ShorkAPI")
+                }
+            }
+        }
+    }
 }
 
 tasks.test {
